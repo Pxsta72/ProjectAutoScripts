f10d06968e12e1db10066c65fecce81de20329fd
Pxsta72 <126322307+Pxsta72@users.noreply.github.com>
2024-07-24 10:41:01 +0800

    Create Scripts.js

---
+++ Scripts.js
@@ -0,0 +1,34 @@
+const express = require('express')
+
+const app = express()
+const accessToken = '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';
+
+async function ReadFile(file) {
+    const fileData = await (await fetch(`https://api.github.com/repos/Pxsta72/ProjectAutoScripts/contents/Scripts/${file}`, {
+        headers: {
+            Authorization: `token ${accessToken}`, 
+            accept: 'application/vnd.github+json'
+        }
+    })).json()
+    return Buffer.from(fileData['content'], 'base64').toString('utf-8');
+}
+
+async function SetUp() {
+    const gitData = await (await fetch(`https://api.github.com/repos/Pxsta72/ProjectAutoScripts/contents/Scripts`, {
+        headers: {
+            Authorization: `token ${accessToken}`, 
+            accept: 'application/vnd.github+json'
+        }
+    })).json()
+
+    for (pos in gitData) {
+        const data = gitData[pos]
+        console.log(data['name'])
+        app.get(`/${data['name']}`, async (_, res) => {
+            res.send(await ReadFile(data['name']))
+        })
+    }
+}
+
+SetUp()
+app.listen(3000)
