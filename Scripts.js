1073782aecb299f9a196e067b0399272e25db49a
Pxsta72 <126322307+Pxsta72@users.noreply.github.com>
2024-08-03 12:09:54 +0800

    Update Scripts.js

--- Scripts.js
+++ Scripts.js
@@ -14,23 +14,6 @@ async function ReadFile(file) {
     return Buffer.from(fileData['content'], 'base64').toString('utf-8');
 }
 
-async function SetUp() {
-    const gitData = await (await fetch(`https://api.github.com/repos/Pxsta72/ProjectAutoScripts/contents/Scripts`, {
-        headers: {
-            Authorization: `token ${accessToken}`, 
-            accept: 'application/vnd.github+json'
-        }
-    })).json()
-
-    for (pos in gitData) {
-        const data = gitData[pos]
-        console.log(data['name'])
-        app.get(`/${data['name']}`, async (_, res) => {
-            res.send(await ReadFile(data['name']))
-        })
-    }
-}
-
 app.get('*', async (req, res) => {
     const page = req.path.substring(1)
     res.send(await ReadFile(page));
